f94f6e9da7c70fb2908ce93a6c6d1d37b21a0f64
Arthur V. Babayan <arthur@babayan.lnx>
2024-04-28 11:16:08 +0400

    Add files

---
+++ DisplayImage.cpp
@@ -0,0 +1,28 @@
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		printf("\n\n\tusage: <application name> <Image Path> \n\n");
+		
+		return -1;
+	}
+
+	cv::Mat img;
+	img = cv::imread(argv[1], 1);
+
+	if (!img.data)
+	{
+		printf("\n\n\tNo image data.\n\n");
+
+		return -1;
+	}
+
+	cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
+	cv::imshow("Display Image",img);
+	cv::waitKey(0);
+
+	return 0;
+};
