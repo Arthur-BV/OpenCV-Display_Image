0ca0113a93fbf0a708f8df672ec47f598de54d75
Arthur V. Babayan <arthur@babayan.lnx>
2024-04-28 12:24:38 +0400

    minor changes

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -5,7 +5,7 @@ int main(int argc, char** argv)
 {
 	if (argc != 2)
 	{
-		printf("\n\n\tusage: <application name> <Image Path> \n\n");
+		printf("\n\n\tusage: <application name> <Image File Path> \n\n");
 		
 		return -1;
 	}
